1e1e2565e86207047e0780b21a06218b460aaaa2
Mandisa Washington <Mandisa.Washington21@brooklyn.cuny.edu>
2017-04-07 14:56:02 -0400

    Initial Commit

---
+++ ZXingSwift/ZXingSwift.h
@@ -0,0 +1,19 @@
+//
+//  ZXingSwift.h
+//  ZXingSwift
+//
+//  Created by Mandisa Washington on 4/7/17.
+//  Copyright © 2017 ZXingSwift Project. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ZXingSwift.
+FOUNDATION_EXPORT double ZXingSwiftVersionNumber;
+
+//! Project version string for ZXingSwift.
+FOUNDATION_EXPORT const unsigned char ZXingSwiftVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ZXingSwift/PublicHeader.h>
+
+
