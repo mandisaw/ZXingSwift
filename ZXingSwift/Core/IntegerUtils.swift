3e023ff19e3d220b6f2653f93d5a5b788e4a1e6e
Mandisa Washington <Mandisa.Washington21@brooklyn.cuny.edu>
2017-08-02 16:06:44 -0400

    Project: Updated Integer forward-compatibility additions (against pending iOS 11). Note that these additions are unfortunately redundant due to the poor generic support for Integer types. If/when this library is updated to iOS 11, it should be possible to refactor this implementation.

--- ZXingSwift/Core/IntegerUtils.swift
+++ ZXingSwift/Core/IntegerUtils.swift
@@ -14,9 +14,111 @@ internal protocol FixedWidthIntegerCompat {
 	
 	static var max : Self { get }
 	static var min : Self { get }
+	
+	func toBitArray (msbFirst: Bool) -> [Bool];
 }
 
 extension Int : FixedWidthIntegerCompat {
+	
+	// TODO 7/20/17 Replace with equivalent built-in property currently in XCode 9 beta
+	var nonzeroBitCount : Int {
+		var result : Int = 0;
+		var value = self;
+		
+		while (value > 0) {
+			if ((value & 0x1) == 1) {
+				result += 1;
+			}
+			
+			value >>= 1;
+		}
+		
+		return result;
+	}
+	
+	// TODO 7/26/17 Replace with equivalent built-in property currently in XCode 9 beta
+	var bitWidth : Int {
+		var result : Int = 0;
+		var value = self;
+		
+		while (value > 0) {
+			result += 1;
+			value >>= 1;
+		}
+		
+		return result;
+	}
+	
+	/** Generates an array of individual bit values, 
+	 in order from least-significant to most-significant, unless stipulated otherwise */
+	func toBitArray (msbFirst: Bool = false) -> [Bool] {
+		var result : [Bool] = Array<Bool>();
+		var value = self;
+		
+		while (value > 0) {
+			result.append ((value & 0x1) == 1);
+			value >>= 1;
+		}
+		
+		if (msbFirst) {
+			result.reverse();
+		}
+		
+		return result;
+	}
+}
+
+extension UInt8 : FixedWidthIntegerCompat {
+	
+	// TODO 7/20/17 Replace with equivalent built-in property currently in XCode 9 beta
+	var nonzeroBitCount : Int {
+		var result : Int = 0;
+		var value = self;
+		
+		while (value > 0) {
+			if ((value & 0x1) == 1) {
+				result += 1;
+			}
+			
+			value >>= 1;
+		}
+		
+		return result;
+	}
+	
+	// TODO 7/26/17 Replace with equivalent built-in property currently in XCode 9 beta
+	var bitWidth : Int {
+		var result : Int = 0;
+		var value = self;
+		
+		while (value > 0) {
+			result += 1;
+			value >>= 1;
+		}
+		
+		return result;
+	}
+	
+	/** Generates an array of individual bit values, 
+	 in order from least-significant to most-significant, unless stipulated otherwise */
+	func toBitArray (msbFirst: Bool = false) -> [Bool] {
+		var result : [Bool] = Array<Bool>();
+		var value = self;
+		
+		while (value > 0) {
+			result.append ((value & 0x1) == 1);
+			value >>= 1;
+		}
+		
+		if (msbFirst) {
+			result.reverse();
+		}
+		
+		return result;
+	}
+}
+
+extension UInt32 : FixedWidthIntegerCompat {
 	
 	// TODO 7/20/17 Replace with equivalent built-in property currently in XCode 9 beta
 	var nonzeroBitCount : Int {
